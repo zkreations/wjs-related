a0292f42713acebf9f7131f521c91c478a445803
Daniel Abel <zeit.p1@gmail.com>
2020-04-28 17:36:19 -0500

    Update

--- README.md
+++ README.md
@@ -8,30 +8,31 @@
 
 ## Introducción
 
-wjs-related es un complemento para Blogger que captura de manera óptima, entradas relacionadas basadas en etiquetas, en **una sola consulta y sin dependencias**. Los complementos que zkreations aporta a blogger llevan el prefijo "wjs" que significa **whale javascript**, ya que pertenecen a nuestro laboratorio de desarrollo, bajo el nombre "**whale**".
+wjs-related es un complemento para Blogger que captura de manera óptima, entradas relacionadas basadas en etiquetas, en **una sola consulta y sin dependencias**.
 
 ## Instalación
 
 Incluir todo el contenido de **includable.xml** dentro de un widget tipo "Blog", posteriormente, utilizar la siguiente etiqueta para incrustar los post relacionados donde se desee:
 
 ```xml
-<b:include name='post-related'/>
+<b:include name='postRelated'/>
 ```
 
-**Tips:** Puedes evitar la carga de post relacionados dentro de la vista previa ya que no se requiere, para ello agregue una condición:
+**Tips:** Puede evitar la carga de post relacionados dentro de la vista previa ya que no se requiere, para ello agregue una condición:
 
 ```xml
-<b:include cond='!data:view.isPreview' name='post-related'/>
+<b:include cond='!data:view.isPreview' name='postRelated'/>
 ```
 
 ## Opciones
 
-| Propiedad    | Tipo |  Valor | 
+| Propiedad    | Tipo |  Valor |
 | ------------ | ---- | ------------------------------------------ |
 | `id`         | string | Código único de una entrada publicada en el blog |
 | `homepage`   | string | Enlace de la página principal. Debe corresponder al id del post |
 | `image`      | image | Url de la imagen por defecto. Se usa cuando no existen imágenes en el post |
 | `length`     | number | Cantidad de entradas relacionadas a mostrar |
+| `localeDate`     | string | Idioma y localidad |
 | `snippet`    | number | Cantidad de texto para el resumen |
 | `imgSize`    | string | Parametros para el tamaño de las imagenes |
 | `container`  | var | Id del Contenedor |
--- source/wjs-related-example.js
+++ source/wjs-related-example.js
@@ -5,106 +5,83 @@
  * Licensed under MIT (github.com/zkreations/wjs-related/blob/master/LICENSE)
  */
 var related = (function(){
- 
-'use strict';
-
-var defaults = {
-   id: '1692379497738760100', // Codigo unico del post
-   homepage: 'https://www.zkreations.com', // Url blog de prueba (debe corresponder a la ID del post)
-   image: 'img/no-img-blogger.png', // Imagen por defecto
-   length: 5, // Cantidad de entradas relacionadas a mostrar
-   snippet: 100, // Cantidad texto para el resumen
-   imgSize: 's256-c', // Cantidad texto para el resumen
-   container: document.getElementById('wjs-related'), // Selector
-   tags: ['plantilla','blogger'] // Etiquetas de prueba
+   'use strict';
+   
+   //Configuracion
+   var defaults = {
+      id: '1692379497738760100',
+      homepage: 'https://www.zkreations.com', //Url del blog
+      image: 'src/imgdefault.jpg', //Imagen por defecto
+      length: 6, //Cantidad de entradas
+      localeDate: 'es-ES', //Idioma
+      snippet: 50, //Cantidad de texto
+      imgSize: 's256', //Recorte de la imagen
+      container: document.getElementById('wjs-related'), //Selector
+      tags: ['tutorial','anime','plantilla'] //Etiquetas
    };
-
+   
+   //<![CDATA[
    var tags$length = defaults.tags.length;
    var script = document.createElement( 'script' );
-
    var src = defaults.homepage + '/feeds/posts/default' +
-   '?alt=json-in-script' +
-   '&callback=related' +
-   '&max-results=' + ( defaults.length + 1 ) +
-   '&q=';
-
+      '?alt=json-in-script' +
+      '&callback=related' +
+      '&max-results=' + ( defaults.length + 1 ) +
+      '&q=';
    for ( var n = 0; n < tags$length; n++ ){
-   src += 'label:"' + defaults.tags[ n ] + '"' + ( n === tags$length - 1 ? '' : '|' );
+      src += 'label:"' + defaults.tags[ n ] + '"' + ( n === tags$length - 1 ? '' : '|' );
    }
-
    script.src = src;
    document.body.appendChild( script );
-
    function render( data ){
-
-   console.log( data ); // Envia a consola los datos json. Eliminar si no se necesita
-
-   var title = data.title.$t;
-
-   var content = data.content;
-
-   var summary = data.summary;
-
-   var body = content ? content.$t : summary.$t;
-
-   var snippet = (body).replace(/<[^>]*>?/g,'').substring( 0, defaults.snippet ) + '...';
-       
-   var img = data.media$thumbnail;
-
-   var tempHtml = document.createElement('div');
-
-   tempHtml.innerHTML = body;
-
-   var imgHtml = tempHtml.querySelector('img');
-
-   var image = ( img ? img.url : (imgHtml ? imgHtml.src : defaults.image)).replace( /s\B\d{2,4}/, defaults.imgSize);
-
-   var url = (function(){
-      for ( var i = 0; i < data.link.length; i++ ){
-      var link = data.link[i];
-      if ( link.rel === 'alternate' ){
-         return link.href;
-      }
-      }
-   })();
-
-   var published = new Date( data.published.$t ).toLocaleDateString(
-      'es-ES',
-      {
-      year:'numeric',
-      month:'long',
-      day: 'numeric'
-      }
-   );
-
-   return (
-      '<div class="wjs-rel">'+
-      '<a href="' + url + '" class="wjs-rel__image">'+
-         '<img src="' + image + '" alt="' + title + '" />'+
-      '</a>'+
-      '<div class="wjs-rel__data">'+
-         '<h3 class="wjs-rel__title">' + title + '</h3>'+
-         '<div class="wjs-rel__date">' + published + '</div>'+
-         '<p class="wjs-rel__snippet">' + snippet + '</p>'+
-      '</div>'+
-      '</div>'
-   );
+      var title = data.title.$t;
+      var content = data.content;
+      var summary = data.summary;
+      var body = content ? content.$t : summary.$t;
+      var snippet = (body).replace(/<[^>]*>?/g,'').substring( 0, defaults.snippet ) + '...';
+      var img = data.media$thumbnail;
+      var tempHtml = document.createElement('div');
+      tempHtml.innerHTML = body;
+      var imgHtml = tempHtml.querySelector('img');
+      var image = ( img ? img.url : (imgHtml ? imgHtml.src : defaults.image)).replace( /s\B\d{2,4}-c/, defaults.imgSize); 
+      var url = (function(){
+         for ( var i = 0; i < data.link.length; i++ ){
+            var link = data.link[i];
+            if ( link.rel === 'alternate' ){
+               return link.href;
+            }
+         }
+      })();
+      var published = new Date( data.published.$t ).toLocaleDateString(
+         defaults.localeDate,
+         {year:'numeric', month:'long', day: 'numeric'}
+      );
+      return (
+         '<div class="relCard">'+
+            '<div class="relCard-content">'+
+               '<a href="' + url + '" class="relCard-image">'+
+                  '<img src="' + image + '" alt="' + title + '" />'+
+               '</a>'+
+               '<div class="relCard-data">'+
+                  '<h3 class="relCard-title">' + title + '</h3>'+
+                  '<div class="relCard-date">' + published + '</div>'+
+                  '<p class="relCard-snippet">' + snippet + '</p>'+
+               '</div>'+
+            '</div>'+
+         '</div>'
+      );
    }
-
    function related( json ){
-   var i = 0;
-   var post;
-   var length = defaults.length;
-
-   for ( ; i < length && ( post = json.feed.entry[ i ] ); i++ ){
-      if ( defaults.id !== post.id.$t.split( '.post-' )[ 1 ] ){
-      defaults.container.innerHTML += render( post );
-      } else {
-      length++;
+      var i = 0;
+      var post;
+      var length = defaults.length;
+      for ( ; i < length && ( post = json.feed.entry[ i ] ); i++ ){
+         if ( defaults.id !== post.id.$t.split( '.post-' )[ 1 ] ){
+            defaults.container.innerHTML += render( post );
+         } else {
+            length++;
+         }
       }
    }
-   }
-
    return related;
-
-   })();
+})();
